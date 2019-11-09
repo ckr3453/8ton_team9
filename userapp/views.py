77f03b4ca8e233da0b37ca09b8b5cd7a14eb5321
ckr3453 <david3453@naver.com>
2019-11-09 23:59:38 +0900

    modified 'views.py' - register class 수정, 닉넴 추가

--- userapp/views.py
+++ userapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, redirect
 from django.views.generic import View
-
+from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib import auth
 # Create your views here.
@@ -16,23 +16,28 @@ class Register(View):
             try:
                 if not User.objects.filter(email=request.POST['email']):
                     user = User.objects.create_user(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'])
+                    nickname = request.POST["nickname"]
+                    profile = Profile(user=user, nickname=nickname)
+                    profile.save()
                     auth.login(request, user)
                 else:
                     raise Exception
+
             except Exception:
                 return render(request, 'register.html')
-            return redirect('index')
+
+            return redirect('base')
         return render(request, 'register.html')
 
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        username = request.POST['email']
         password = request.POST['pass']
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('index')
+            return redirect('base')
         else:
             return render(request, 'login.html', {'error': '아이디 혹은 패스워드가 맞지 않습니다.'})
     else:

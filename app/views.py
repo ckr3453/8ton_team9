c58e85ed80a7c322a13f673a2766e003a39a2135
Seojin Kim <1106laura@naver.com>
2019-11-09 22:58:10 +0900

    third commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'min1/home.html')
 
 def index(request):
     return render(request, 'min1/index.html')    
+
+def write(request):
+    return render(request, 'min1/write.html')
+
+def detail(request):
+    
+    return render(request, 'min1/detail.html')

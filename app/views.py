0d3bea38d8a917f3e09f999c8dc9cf121f2eadb6
Seojin Kim <1106laura@naver.com>
2019-11-10 00:00:53 +0900

    파일연결, 멘트

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+
 def home(request):
     return render(request, 'min1/home.html')
 
@@ -10,5 +11,4 @@ def write(request):
     return render(request, 'min1/write.html')
 
 def detail(request):
-    
     return render(request, 'min1/detail.html')
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
